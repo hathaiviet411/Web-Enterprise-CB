6abe7c61ea347ad394c2f62c1d58b8fe1efe86a4
Ha Thai Viet <hathaiviet411@gmail.com>
2022-03-15 22:11:57 +0700

    [FE][Thaiviet] - Inital Base.

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-navigation-drawer v-model="drawer" app>
+    <v-img
+      height="140"
+      class="pa-4"
+      src="@/assets/images/logo.png"
+    >
+    </v-img>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-item v-for="[icon, text] in links" :key="icon" link>
+        <v-list-item-icon>
+          <v-icon>{{ icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ text }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  props: ['drawer'],
+  data() {
+    return {
+      links: [
+        ["mdi-microsoft-windows", "Dashboard"],
+        ["mdi-account", "Profile"],
+        ["mdi-clipboard-list-outline", "Products"],
+        ["mdi-card-account-details-outline", "Orders"],
+        ["mdi-cog", "System Setting"],
+      ],
+      drawerStorage: this.drawer,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
