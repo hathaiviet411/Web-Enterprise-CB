bdb5022c2ec56d4043e8d51cd1514ab35b36beae
Ha Thai Viet <hathaiviet411@gmail.com>
2022-03-18 20:48:02 +0700

    [FE][Dev] - Upadte.

--- src/api/modules/category.js
+++ src/api/modules/category.js
@@ -3,3 +3,7 @@ import * as RequestApi from '../request';
 export function getCategory(url, data) {
     return RequestApi.getAll(url, data);
 }
+
+export function deleteCategory(url, category_id) {
+    return RequestApi.deleteOne(url, category_id);
+}
--- src/pages/CategoryManagement/index.vue
+++ src/pages/CategoryManagement/index.vue
@@ -1,137 +1,154 @@
 <template>
 	<div>
-
 		<div class="header">
 			<v-card class="mx-auto my-12" min-width="100%">
-				<v-card-title>{{ 'Category Management Filter' }}</v-card-title>
-				<v-card-text>
-					<v-container>
-						<v-row>
-							<v-col lg="1" sm="1" md="1">
-								<v-switch v-model="filter.isCheck.category_name" color="purple" />
-							</v-col>
-							<v-col lg="11" sm="11" md="11">
-								<v-text-field
-									v-model="filter.category_name"
-									label="Category Name"
-									clearable
-									:disabled="!filter.isCheck.category_name"
-									prepend-inner-icon="mdi-library"
-								/>
-							</v-col>
-						</v-row>
+				<v-expansion-panels>
+					<v-expansion-panel>
+						<v-expansion-panel-header>
+							<v-card-title>
+								<v-row>
+									<v-col lg="1" sm="1" md="1">
+										<v-icon left style="color: purple;">mdi-filter</v-icon>
+									</v-col>
+									<v-col lg="11" sm="11" md="11">
+										<span>{{ 'Filter' }}</span>
+									</v-col>
+								</v-row>
+							</v-card-title>
+						</v-expansion-panel-header>
 
-						<v-row>
-							<v-col lg="1" sm="1" md="1">
-								<v-switch v-model="filter.isCheck.created_date" color="purple" />
-							</v-col>
-							<v-col lg="11" sm="11" md="11">
-								<v-menu
-									ref="created_date_menu"
-									v-model="created_date_menu"
-									:close-on-content-click="false"
-									transition="scale-transition"
-									offset-y
-									max-width="290px"
-									min-width="auto"
-								>
-									<template v-slot:activator="{ on, attrs }">
-										<v-text-field
-											:value="filter.created_date"
-											label="Created Date"
-											hint="YYYY-mm-dd"
-											persistent-hint
-											prepend-inner-icon="mdi-calendar"
-											v-bind="attrs"
-											clearable
-											readonly
-											v-on="on"
-										/>
-									</template>
-									<v-date-picker
-										v-if="filter.isCheck.created_date === true"
-										v-model="filter.created_date"
-										no-title
-										:max="filter.expired_date"
-										@input="created_date_menu = false"
-									/>
-								</v-menu>
-							</v-col>
-						</v-row>
+						<v-expansion-panel-content>
+							<v-card-text>
+								<v-container>
+									<v-row>
+										<v-col lg="1" sm="1" md="1">
+											<v-switch v-model="filter.isCheck.category_name" color="purple" />
+										</v-col>
+										<v-col lg="11" sm="11" md="11">
+											<v-text-field
+												v-model="filter.category_name"
+												label="Category Name"
+												clearable
+												:disabled="!filter.isCheck.category_name"
+												prepend-inner-icon="mdi-library"
+											/>
+										</v-col>
+									</v-row>
 
-						<v-row>
-							<v-col lg="1" sm="1" md="1">
-								<v-switch v-model="filter.isCheck.expired_date" color="purple" />
-							</v-col>
-							<v-col lg="11" sm="11" md="11">
-								<v-menu
-									ref="expired_date_menu"
-									v-model="expired_date_menu"
-									:close-on-content-click="false"
-									transition="scale-transition"
-									offset-y
-									max-width="290px"
-									min-width="auto"
-								>
-									<template v-slot:activator="{ on, attrs }">
-										<v-text-field
-											:value="filter.expired_date"
-											label="Expired Date"
-											hint="YYYY-mm-dd"
-											persistent-hint
-											prepend-inner-icon="mdi-calendar"
-											v-bind="attrs"
-											clearable
-											readonly
-											v-on="on"
-										/>
-									</template>
-									<v-date-picker
-										v-if="filter.isCheck.expired_date === true"
-										v-model="filter.expired_date"
-										no-title
-										:min="filter.created_date"
-										@input="expired_date_menu = false"
-									/>
-								</v-menu>
-							</v-col>
-						</v-row>
+									<v-row>
+										<v-col lg="1" sm="1" md="1">
+											<v-switch v-model="filter.isCheck.created_date" color="purple" />
+										</v-col>
+										<v-col lg="11" sm="11" md="11">
+											<v-menu
+												ref="created_date_menu"
+												v-model="created_date_menu"
+												:close-on-content-click="false"
+												transition="scale-transition"
+												offset-y
+												max-width="290px"
+												min-width="auto"
+											>
+												<template v-slot:activator="{ on, attrs }">
+													<v-text-field
+														:value="filter.created_date"
+														label="Created Date"
+														hint="YYYY-mm-dd"
+														persistent-hint
+														prepend-inner-icon="mdi-calendar"
+														v-bind="attrs"
+														clearable
+														readonly
+														v-on="on"
+													/>
+												</template>
+												<v-date-picker
+													v-if="filter.isCheck.created_date === true"
+													v-model="filter.created_date"
+													no-title
+													:max="filter.expired_date"
+													@input="created_date_menu = false"
+												/>
+											</v-menu>
+										</v-col>
+									</v-row>
 
-						<v-row>
-							<v-col cols="12">
-								<v-btn
-									color="primary"
-									class="mx-2"
-									@click="doFilter()"
-								>
-									<v-icon left>mdi-magnify</v-icon>
-									<span>{{ 'Apply' }}</span>
-								</v-btn>
-								<v-btn
-									color="error"
-									class="mx-2"
-									@click="resetFilter()"
-								>
-									<v-icon left>mdi-eraser</v-icon>
-									<span>{{ 'Reset' }}</span>
-								</v-btn>
-							</v-col>
-						</v-row>
-					</v-container>
-				</v-card-text>
+									<v-row>
+										<v-col lg="1" sm="1" md="1">
+											<v-switch v-model="filter.isCheck.expired_date" color="purple" />
+										</v-col>
+										<v-col lg="11" sm="11" md="11">
+											<v-menu
+												ref="expired_date_menu"
+												v-model="expired_date_menu"
+												:close-on-content-click="false"
+												transition="scale-transition"
+												offset-y
+												max-width="290px"
+												min-width="auto"
+											>
+												<template v-slot:activator="{ on, attrs }">
+													<v-text-field
+														:value="filter.expired_date"
+														label="Expired Date"
+														hint="YYYY-mm-dd"
+														persistent-hint
+														prepend-inner-icon="mdi-calendar"
+														v-bind="attrs"
+														clearable
+														readonly
+														v-on="on"
+													/>
+												</template>
+												<v-date-picker
+													v-if="filter.isCheck.expired_date === true"
+													v-model="filter.expired_date"
+													no-title
+													:min="filter.created_date"
+													@input="expired_date_menu = false"
+												/>
+											</v-menu>
+										</v-col>
+									</v-row>
+								</v-container>
+							</v-card-text>
+
+							<v-card-actions>
+								<v-row>
+									<v-col cols="12">
+										<v-btn
+											color="primary"
+											class="mx-2"
+											@click="doFilter()"
+										>
+											<v-icon left>mdi-magnify</v-icon>
+											<span>{{ 'Apply' }}</span>
+										</v-btn>
+										<v-btn
+											color="error"
+											class="mx-2"
+											@click="resetFilter()"
+										>
+											<v-icon left>mdi-eraser</v-icon>
+											<span>{{ 'Reset' }}</span>
+										</v-btn>
+									</v-col>
+								</v-row>
+							</v-card-actions>
+						</v-expansion-panel-content>
+					</v-expansion-panel>
+				</v-expansion-panels>
 			</v-card>
 		</div>
 
 		<v-data-table
-			:headers="headers"
-			:items="desserts"
-			sort-by="calories"
+			:headers="vFields"
+			:items="vItems"
+			sort-by="categoryName"
 			class="elevation-12"
 		>
 			<template v-slot:top>
-				<v-toolbar
-					flat
-				>
+				<v-toolbar flat>
 					<v-toolbar-title>Category Management</v-toolbar-title>
 
 					<v-divider class="mx-4" inset vertical />
@@ -145,44 +162,36 @@
 
 					<v-dialog v-model="dialogDelete" max-width="500px">
 						<v-card>
-							<v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+							<v-card-title>
+								<v-row>
+									<v-col cols="12" class="text-center">
+										<span>Are you sure to delete?</span>
+									</v-col>
+								</v-row>
+							</v-card-title>
 
 							<v-card-text>
 								<v-container>
 									<v-row>
 										<v-col cols="12" sm="12" md="12">
 											<v-text-field
-												:value="editedItem.name"
-												label="Dessert name"
-												disabled
+												:value="tmpStoreData.category_name"
+												label="Category Name"
+												readonly
 											/>
 										</v-col>
 										<v-col cols="12" sm="12" md="12">
 											<v-text-field
-												:value="editedItem.calories"
-												label="Calories"
-												disabled
+												:value="tmpStoreData.created_date"
+												label="Created Date"
+												readonly
 											/>
 										</v-col>
 										<v-col cols="12" sm="12" md="12">
 											<v-text-field
-												:value="editedItem.fat"
-												label="Fat (g)"
-												disabled
-											/>
-										</v-col>
-										<v-col cols="12" sm="12" md="12">
-											<v-text-field
-												:value="editedItem.carbs"
-												label="Carbs (g)"
-												disabled
-											/>
-										</v-col>
-										<v-col cols="12" sm="12" md="12">
-											<v-text-field
-												:value="editedItem.protein"
-												label="Protein (g)"
-												disabled
+												:value="tmpStoreData.expired_date"
+												label="Expired Date"
+												readonly
 											/>
 										</v-col>
 									</v-row>
@@ -192,12 +201,12 @@
 							<v-card-actions>
 								<v-spacer />
 
-								<v-btn tile color="success" @click="closeDelete">
+								<v-btn tile color="success" class="btn" @click="closeDelete()">
 									<v-icon left>mdi-delete-empty</v-icon>
 									<span>Cancel</span>
 								</v-btn>
 
-								<v-btn tile color="success" @click="deleteItemConfirm">
+								<v-btn tile color="success" class="btn confirm" @click="deleteItemConfirm()">
 									<v-icon left>mdi-delete-empty</v-icon>
 									<span>OK</span>
 								</v-btn>
@@ -209,12 +218,8 @@
 			</template>
 
 			<template v-slot:[`item.actions`]="{ item }">
-				<v-icon small class="mr-2" @click="editItem(item)">
-					mdi-pencil
-				</v-icon>
-				<v-icon small @click="deleteItem(item)">
-					mdi-delete
-				</v-icon>
+				<v-icon normal class="mr-2" style="color: #542E71;">mdi-pencil</v-icon>
+				<v-icon normal style="color: #CF0000;" @click="deleteItem(item)">mdi-delete</v-icon>
 			</template>
 
 			<template v-slot:no-data>
@@ -225,14 +230,19 @@
 </template>
 
 <script>
-import { getCategory } from '@/api/modules/category';
+import { getCategory, deleteCategory } from '@/api/modules/category';
+
+import { convertDateToISO } from '@/utils/handleConvertDateFormat';
 
 const URL_GET_CATEGORY = '/admin/category';
+const URL_DELETE_CATEGORY = '/qamanager/category';
 
 export default {
     name: 'CategoryManagement',
     data() {
         return {
+            convertDateToISO: convertDateToISO,
+
             filter: {
                 category_name: '',
                 created_date: '',
@@ -244,48 +254,31 @@ export default {
                     expired_date: false,
                 },
             },
-            dialog: false,
+
             dialogDelete: false,
-            headers: [
-                {
-                    text: 'Dessert (100g serving)',
-                    align: 'start',
-                    sortable: false,
-                    value: 'name',
-                },
-                { text: 'Calories', value: 'calories' },
-                { text: 'Fat (g)', value: 'fat' },
-                { text: 'Carbs (g)', value: 'carbs' },
-                { text: 'Protein (g)', value: 'protein' },
-                { text: 'Actions', value: 'actions', sortable: false },
+
+            vFields: [
+                { text: 'Category Name', value: 'categoryName', sortable: true, align: 'center' },
+                { text: 'Created Date', value: 'firstClosureDate', sortable: true, align: 'center' },
+                { text: 'Expired Date', value: 'finalClosureDate', sortable: true, align: 'center' },
+                { text: 'Actions', value: 'actions', sortable: false, align: 'center' },
             ],
-            desserts: [],
-            editedIndex: -1,
-            editedItem: {
-                name: '',
-                calories: 0,
-                fat: 0,
-                carbs: 0,
-                protein: 0,
-            },
-            defaultItem: {
-                name: '',
-                calories: 0,
-                fat: 0,
-                carbs: 0,
-                protein: 0,
-            },
+
+            vItems: [],
 
             created_date_menu: false,
             expired_date_menu: false,
+
+            tmpStoreData: {
+                id: '',
+                category_name: '',
+                created_date: '',
+                expired_date: '',
+            },
         };
     },
 
     computed: {
-        formTitle() {
-            return this.editedIndex === -1 ? 'New Item' : 'Edit Item';
-        },
-
         isCheckCategoryName() {
             return this.filter.isCheck.category_name;
         },
@@ -300,13 +293,6 @@ export default {
     },
 
     watch: {
-        dialog(val) {
-            val || this.close();
-        },
-        dialogDelete(val) {
-            val || this.closeDelete();
-        },
-
         isCheckCategoryName() {
             this.filter.category_name = '';
         },
@@ -321,7 +307,6 @@ export default {
     },
 
     created() {
-        this.initialize();
         this.getCategoryData();
     },
 
@@ -329,131 +314,53 @@ export default {
         async getCategoryData() {
             try {
                 const response = await getCategory(URL_GET_CATEGORY);
-                console.log(response);
+                if (response.status === true) {
+                    const RAW_DATA = [...response.category];
+                    for (let i = 0; i < RAW_DATA.length; i++) {
+                        RAW_DATA[i].firstClosureDate = convertDateToISO(RAW_DATA[i].firstClosureDate);
+                        RAW_DATA[i].finalClosureDate = convertDateToISO(RAW_DATA[i].finalClosureDate);
+                    }
+                    this.vItems = RAW_DATA;
+                }
             } catch (error) {
                 console.log(error.message);
             }
         },
 
-        initialize() {
-            this.desserts = [
-                {
-                    name: 'Frozen Yogurt',
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
-                },
-                {
-                    name: 'Ice cream sandwich',
-                    calories: 237,
-                    fat: 9.0,
-                    carbs: 37,
-                    protein: 4.3,
-                },
-                {
-                    name: 'Eclair',
-                    calories: 262,
-                    fat: 16.0,
-                    carbs: 23,
-                    protein: 6.0,
-                },
-                {
-                    name: 'Cupcake',
-                    calories: 305,
-                    fat: 3.7,
-                    carbs: 67,
-                    protein: 4.3,
-                },
-                {
-                    name: 'Gingerbread',
-                    calories: 356,
-                    fat: 16.0,
-                    carbs: 49,
-                    protein: 3.9,
-                },
-                {
-                    name: 'Jelly bean',
-                    calories: 375,
-                    fat: 0.0,
-                    carbs: 94,
-                    protein: 0.0,
-                },
-                {
-                    name: 'Lollipop',
-                    calories: 392,
-                    fat: 0.2,
-                    carbs: 98,
-                    protein: 0,
-                },
-                {
-                    name: 'Honeycomb',
-                    calories: 408,
-                    fat: 3.2,
-                    carbs: 87,
-                    protein: 6.5,
-                },
-                {
-                    name: 'Donut',
-                    calories: 452,
-                    fat: 25.0,
-                    carbs: 51,
-                    protein: 4.9,
-                },
-                {
-                    name: 'KitKat',
-                    calories: 518,
-                    fat: 26.0,
-                    carbs: 65,
-                    protein: 7,
-                },
-            ];
-        },
-
-        editItem(item) {
-            this.editedIndex = this.desserts.indexOf(item);
-            this.editedItem = Object.assign({}, item);
-            this.dialog = true;
+        doFilter() {
+            //
         },
 
         deleteItem(item) {
-            this.editedIndex = this.desserts.indexOf(item);
-            this.editedItem = Object.assign({}, item);
             this.dialogDelete = true;
-        },
 
-        deleteItemConfirm() {
-            this.desserts.splice(this.editedIndex, 1);
-            this.closeDelete();
-        },
-
-        close() {
-            this.dialog = false;
-            this.$nextTick(() => {
-                this.editedItem = Object.assign({}, this.defaultItem);
-                this.editedIndex = -1;
-            });
+            this.tmpStoreData = {
+                id: item._id,
+                category_name: item.categoryName,
+                created_date: item.firstClosureDate,
+                expired_date: item.finalClosureDate,
+            };
         },
 
         closeDelete() {
             this.dialogDelete = false;
-            this.$nextTick(() => {
-                this.editedItem = Object.assign({}, this.defaultItem);
-                this.editedIndex = -1;
-            });
+
+            this.tmpStoreData = {
+                id: '',
+                category_name: '',
+                created_date: '',
+                expired_date: '',
+            };
         },
 
-        save() {
-            if (this.editedIndex > -1) {
-                Object.assign(this.desserts[this.editedIndex], this.editedItem);
-            } else {
-                this.desserts.push(this.editedItem);
+        async deleteItemConfirm() {
+            try {
+                const URL = `${URL_DELETE_CATEGORY}/${this.tmpStoreData.id}`;
+                const response = await deleteCategory(URL);
+                console.log(response);
+            } catch (error) {
+                console.log(error.message);
             }
-            this.close();
-        },
-
-        doFilter() {
-            //
         },
 
         resetFilter() {
@@ -468,5 +375,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @import '@/scss/variables.scss';
 
+    .btn {
+        min-width: 110px !important;
+        font-weight: bold;
+    }
+
+    .confirm {
+        background-color: #FF1818 !important;
+        border: none !important;
+    }
 </style>
