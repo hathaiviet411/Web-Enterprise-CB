eef672122ca4703d213b2114f17504899385fe39
thanhan7501 <71816459+thanhan7501@users.noreply.github.com>
2022-03-24 01:02:15 +0700

    create comment

--- controllers/main/comment.js
+++ controllers/main/comment.js
@@ -1,1 +1,24 @@
+const Comment = require("../../models/comment");
+
 const socket = io();
+
+module.exports = {
+    createComment: async (ctx) => {
+        const comment = new Comment(ctx.request.body)
+        const user = ctx.state.user;
+        comment.user = user.user._id
+        await comment.save()
+
+        return (ctx.body = {
+            status: true
+        })
+    },
+
+    deleteComment: async (ctx) => {
+
+    },
+
+    updateComment: async (ctx) => {
+
+    },
+}
--- routes/api/main/comment.js
+++ routes/api/main/comment.js
@@ -1,5 +1,11 @@
 const Router = require("@koa/router");
 const router = new Router();
+const controller = require("../../../controllers/main/comment")
 
+router.post("/comment", controller.createComment)
+
+router.put("/comment/:id", controller.updateComment)
+
+router.delete("/comment/:id", controller.deleteComment)
 
 module.exports = router.routes();
