98ea0220c98638633ff31b70a1c9953e074f0e4d
Ha Thai Viet <hathaiviet411@gmail.com>
2022-04-02 15:28:47 +0700

    [FE]

--- src/pages/Dashboard/detail.vue
+++ src/pages/Dashboard/detail.vue
@@ -35,9 +35,9 @@
 
 						<v-col cols="12" lg="3">
 							<div class="author-info">
-								<span class="author-name">{{ DATA.user + ' - ' + post.user.username }}</span>
+								<span class="author-name">{{ DATA.idea.user.name + ' - ' + DATA.idea.user.username }}</span>
 								<br>
-								<span class="uploaded-time">{{ post.createdAt | moment('from') }}
+								<span class="uploaded-time">{{ DATA.idea.user.createdAt | moment('from') }}
 									<span>·</span>
 									<v-icon class="pl-1" color="#999" small>mdi-earth</v-icon>
 								</span>
@@ -51,7 +51,7 @@
 										<strong>
 											<i class="fas fa-building mr-3" />Department:
 										</strong>
-										{{ post.department.departmentName }}
+										{{ DATA.idea.department.departmentName }}
 									</span>
 								</v-col>
 
@@ -60,7 +60,7 @@
 										<strong>
 											<i class="fas fa-box-open mr-3" />Category:
 										</strong>
-										{{ post.category.categoryName }}
+										{{ DATA.idea.category.categoryName }}
 									</span>
 								</v-col>
 							</v-row>
@@ -68,13 +68,158 @@
 					</v-row>
 				</v-card-title>
 
-				<v-card-text>
-					Text
+				<v-card-text v-if="DATA.idea.ideaPicture" class="content">
+					<v-row>
+						<v-col cols="12">
+							<h5 class="post-content ml-3">{{ DATA.idea.ideaTitle }}</h5>
+						</v-col>
+						<v-col cols="12">
+							<h5 class="post-content ml-3">{{ DATA.idea.ideaContent }}</h5>
+						</v-col>
+					</v-row>
+
+					<div class="text-center">
+						<v-img
+							width="1168"
+							height="472"
+							contain
+							:src="DATA.idea.ideaPicture"
+						/>
+					</div>
+				</v-card-text>
+
+				<v-card-text v-else class="random-background" :style="setBg()">
+					<div class="text-center">
+						<v-row>
+							<v-col cols="12">
+								<h5 class="post-content">{{ DATA.idea.ideaTitle }}</h5>
+							</v-col>
+							<v-col cols="12">
+								<h5 class="post-content">{{ DATA.idea.ideaContent }}</h5>
+							</v-col>
+						</v-row>
+					</div>
 				</v-card-text>
 
 				<v-card-actions>
-					Actions
+					<v-row class="mt-3">
+						<v-col cols="3" lg="3" class="text-center">
+							<v-icon class="p-1" style="margin-bottom: 2px;">mdi-thumb-up</v-icon>
+							<span class="like-total text-underline">{{ DATA.likes + ' likes' }}</span>
+						</v-col>
+
+						<v-col cols="3" lg="3" class="text-center">
+							<v-icon class="p-1">mdi-eye</v-icon>
+							<span class="comment-total text-underline">{{ DATA.idea.viewCount + ' views' }}</span>
+						</v-col>
+
+						<v-col cols="3" lg="3" class="text-center">
+							<v-icon class="p-1">mdi-message</v-icon>
+							<span class="comment-total text-underline">{{ (DATA.comments.length === 0 ? 0 : (post.comments.length + 1)) + ' comments' }}</span>
+						</v-col>
+
+						<v-col cols="3" lg="3" class="text-center">
+							<v-icon class="p-1" style="margin-bottom: 2px;">mdi-thumb-down</v-icon>
+							<span class="share-total text-underline mr-3">{{ DATA.dislikes + ' dislike' }}</span>
+						</v-col>
+					</v-row>
 				</v-card-actions>
+
+				<v-divider class="mr-3 ml-3" />
+
+				<v-row class="ml-1">
+					<v-col cols="3" lg="4" class="text-center pl-0 pr-0">
+
+						<v-btn x-small class="card-button">
+							<v-icon small color="black">mdi-thumb-up</v-icon>
+							<span class="button-text">Like</span>
+						</v-btn>
+
+					</v-col>
+
+					<v-col cols="4" lg="4" class="text-center pl-0 pr-0">
+						<v-btn x-small class="card-button" @click="isShowCommentSector = !isShowCommentSector">
+							<v-icon small>mdi-message</v-icon>
+							<span class="button-text">Comment</span>
+						</v-btn>
+					</v-col>
+
+					<v-col cols="4" lg="4" class="text-center pl-0 pr-0">
+						<v-btn x-small class="card-button">
+							<v-icon small>mdi-thumb-down</v-icon>
+							<span class="button-text">Dislike</span>
+						</v-btn>
+					</v-col>
+				</v-row>
+
+				<v-divider class="mr-3 ml-3" />
+
+				<Transition appear name="fade" mode="in-out">
+					<div v-if="isShowCommentSector">
+						<v-row>
+							<v-col cols="2" lg="1">
+								<v-img
+									min-width="50"
+									min-height="50"
+									max-width="60"
+									max-height="60"
+									contain
+									class="rounded-circle ml-3"
+									src="@/assets/images/default_avatar.png"
+								/>
+							</v-col>
+
+							<v-col cols="10" lg="11">
+								<v-text-field
+									label="Viết bình luận..."
+									filled
+									hide-details
+									rounded
+									dense
+									class="mr-3 ml-3"
+								/>
+							</v-col>
+						</v-row>
+
+						<div v-for="(comment, indexComment) in DATA.comments" :key="indexComment">
+							<v-row>
+								<v-col cols="2" lg="1">
+									<v-img
+										min-width="50"
+										min-height="50"
+										max-width="60"
+										max-height="60"
+										contain
+										class="rounded-circle ml-3"
+										:src="'https://joeschmoe.io/api/v1/random'"
+									/>
+								</v-col>
+
+								<v-col cols="10" lg="11">
+									<v-textarea
+										readonly
+										filled
+										:label="comment.author"
+										rounded
+										dense
+										hide-details
+										rows="1"
+										auto-grow
+										class="mr-3 ml-3 comment-section"
+										:value="comment.content"
+									/>
+
+									<div class="comment-section-button ml-6 mt-1">
+										<span class="text-underline">Like</span>
+										<span class="ml-3 text-underline">Trả lời</span>
+										<span class="ml-3 text-underline">{{ comment.uploadedTime }}</span>
+									</div>
+								</v-col>
+							</v-row>
+						</div>
+
+					</div>
+				</Transition>
 			</v-card>
 
 		</b-overlay>
@@ -103,6 +248,8 @@ export default {
                 blur: '1rem',
                 rounded: 'sm',
             },
+
+            isShowCommentSector: false,
         };
     },
     created() {
@@ -115,17 +262,121 @@ export default {
                 const response = await getOneIdea(URL);
 
                 if (response.status === true) {
-                    this.DATA = response.idea;
+                    this.DATA = response.data;
                 }
                 console.log(response);
             } catch (error) {
                 console.log(error);
             }
         },
+
+        setBg() {
+            const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            const style = '#' + randomColor;
+            return `background-color: ${style}`;
+        },
     },
 };
 </script>
 
 <style lang="scss" scoped>
+@import '@/scss/variables.scss';
 
+.dashboard {
+	background-image: url('/assets/images/background.png');
+	background-repeat: no-repeat;
+	background-size: auto;
+}
+
+.author-name {
+	font-size: 14px;
+	font-weight: bolder;
+}
+
+.uploaded-time {
+	font-size: 14px;
+	color: #999;
+}
+
+.content {
+	display: flex;
+	justify-content: center;
+	align-content: center;
+	flex-direction: column;
+	height: 472px;
+}
+
+.post-content {
+	color: $black;
+	line-height: 40px;
+}
+
+.like-total {
+	font-size: 12px;
+	color: #999;
+}
+
+.comment-total {
+	font-size: 12px;
+	color: #999;
+}
+
+.share-total {
+	font-size: 12px;
+	color: #999;
+}
+
+.text-underline:hover {
+	text-decoration: underline;
+}
+
+.button-text {
+	margin-left: 10px;
+	font-size: 10px;
+}
+
+.card-button {
+	height: 40px !important;
+	box-shadow: none;
+	border: 1px solid $white !important;
+	background-color: #FFFFFF !important;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+
+.comment-section-button {
+	font-size: 12px;
+}
+
+.comment-section {
+	font-size: 14px;
+}
+
+.btn-loading {
+	opacity: .8;
+}
+
+.text-small {
+	font-size: 12px;
+}
+
+.random-background {
+	display: flex;
+	justify-content: center;
+	align-content: center;
+	flex-direction: column;
+	height: 472px;
+}
+
+.overlay-card {
+	margin-top: 100px;
+}
 </style>
