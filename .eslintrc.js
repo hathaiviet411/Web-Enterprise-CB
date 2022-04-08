f1c5ce9bd84a5b34b12e98c6929496cf4a3c4bf2
Ha Thai Viet <hathaiviet411@gmail.com>
2022-04-08 23:49:05 +0700

    [FE]

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
         browser: true,
         node: true,
         es6: true,
+        jest: true,
     },
     extends: ['plugin:vue/recommended', 'eslint:recommended'],
     rules: {
