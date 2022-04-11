61a32fb3edd99fc5bf45e13896b9ab5bea2a9d8a
Ha Thai Viet <hathaiviet411@gmail.com>
2022-04-11 09:36:43 +0700

    FE backup

--- src/tests/Login/Login.spec.js
+++ src/tests/Login/Login.spec.js
@@ -3,36 +3,35 @@ import Vue from '../../pages/DepartmentManagement/index.vue';
 import Vuetify from 'vuetify';
 
 describe('Example', () => {
-  const localVue = createLocalVue();
-  let vuetify;
+    const localVue = createLocalVue();
+    let vuetify;
 
-  beforeEach(() => {
-    vuetify = new Vuetify();
-  });
+    beforeEach(() => {
+        vuetify = new Vuetify();
+    });
 
-  it('Test Login', () => {
-      expect(1 + 1).toEqual(2);
-  });
+    it('Test Login', () => {
+        expect(1 + 1).toEqual(2);
+    });
 
-  it('Test Dashboard', () => {
-      expect(1 + 1).toEqual(2);
-  });
+    it('Test Dashboard', () => {
+        expect(1 + 1).toEqual(2);
+    });
 
-  it('Test User Management', () => {
-      expect(1 + 1).toEqual(2);
-  });
+    it('Test User Management', () => {
+        expect(1 + 1).toEqual(2);
+    });
 
-  it('Test Idea Management', () => {
-      expect(1 + 1).toEqual(2);
-  });
+    it('Test Idea Management', () => {
+        expect(1 + 1).toEqual(2);
+    });
 
-  it('Test Cat', () => {
-      const wrapper = shallowMount(Vue, {
-          localVue,
-          vuetify
-      });
+    it('Test Cat', () => {
+        const wrapper = shallowMount(Vue, {
+            localVue,
+            vuetify,
+        });
 
-      expect(wrapper.find('.cat').exists()).toBe(true);
-
-  });
+        expect(wrapper.find('.cat').exists()).toBe(true);
+    });
 });
