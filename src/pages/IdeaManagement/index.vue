f05a9923b152ec5a36fd325cbd64cf4c45dba369
Ha Thai Viet <hathaiviet411@gmail.com>
2022-04-13 21:20:13 +0700

    [FE]

--- src/pages/IdeaManagement/index.vue
+++ src/pages/IdeaManagement/index.vue
@@ -763,15 +763,14 @@ export default {
                         });
                         this.dialogCreateIdea = false;
                         this.getListIdea();
-                    }
-					else if (response.status === false) {
-						MakeToast({
+                    } else if (response.status === false) {
+                        MakeToast({
                             variant: 'warning',
                             title: 'Warning',
                             content: `${response.message}`,
                         });
                         this.dialogCreateIdea = false;
-					}
+                    }
                 } catch (error) {
                     console.log(error.message);
                 }
