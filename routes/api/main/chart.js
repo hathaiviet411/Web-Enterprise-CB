14210255611ae205f4e02c43a6c02fdf94748d5a
Ngo Van Loi <loivngoo@gmail.com>
2022-04-13 20:16:13 +0700

    api dashboard created

---
+++ routes/api/main/chart.js
@@ -0,0 +1,9 @@
+const Router = require("@koa/router");
+const router = new Router();
+const controller = require("../../../controllers/main/chart");
+
+router.get("/chart/list-ideas-have-most-view", controller.getListIdeasHaveMostView)
+
+router.get("/chart/get-dashboard-likes", controller.getDashboardLikes)
+
+module.exports = router.routes();
--- routes/api/main/index.js
+++ routes/api/main/index.js
@@ -6,8 +6,10 @@ const category = require("./category");
 const department = require("./department");
 const role = require("./role");
 const like = require("./like");
+const chart = require("./chart")
 
 router.use(user);
+router.use(chart)
 router.use(category);
 router.use(department);
 router.use(role);
