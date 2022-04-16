8ba6aaa2dbee4f95762067648a34d884e387ad7b
Ngo Van Loi <loivngoo@gmail.com>
2022-04-16 09:57:25 +0700

    refactor service api

--- controllers/main/service.js
+++ controllers/main/service.js
@@ -1,5 +1,8 @@
 const { parse } = require('json2csv');
+
+
 const axios = require("axios");
+
 const AdmZip = require('adm-zip');
 const fs = require('fs');
 const { cwd } = require("process");
@@ -12,7 +15,7 @@ const Department = require('../../models/department');
 module.exports = {
     async downloadCsv(ctx) {
 
-        let { category_id } = ctx.request.body;
+        let { category_id } = ctx.request.body
         const category = await Category.findOne({ _id: category_id }).lean()
 
         let ideas = await Idea.find({ category: category._id }).lean()
@@ -48,7 +51,9 @@ module.exports = {
 
         const slug = new Date().getFullYear()
 
-        let file_name = `${slug}_${Date.now()}.csv`
+        let file_name = `${slug}_${Date.now().toString()}.csv`
+
+        console.log(file_name);
 
         var path = `${directoryPath}/public/csv/${file_name}`;
         fs.writeFile(path, csv, 'utf8', function (err, data) {
@@ -59,28 +64,33 @@ module.exports = {
         const response = fs.createReadStream(path);
         ctx.response.set("content-type", 'text/csv');
         ctx.response.set("content-disposition", `attachment; filename=${file_name}`);
-        const downloadUrl = `${process.env.BASE_URL}/csv/${file_name}`
-        ctx.body = downloadUrl;
+        ctx.body = response;
     },
 
     async downloadZip(ctx) {
-        const { path } = ctx.request.body;
-        console.log(ctx.request.body)
-
-        const slug = new Date().getFullYear()
-
-        let file_name = `${slug}_${Date.now()}.zip`
-
         const zip = new AdmZip();
 
-        const url = path;
-        const body = await axios.get(url, {
-            responseType: 'arraybuffer'
-        });
-        const data = body.data;
+        const { path } = ctx.request.body;
+        const slug = new Date().getFullYear()
 
-        zip.addFile(`item`, Buffer.from(data, "utf8"), "download zip folder", 0644 << 16)
-        zip.toBuffer()
+        const file_name = `${slug}_${Date.now()}.zip`
+        let index = 1
+
+        for (let pathAPI of path) {
+
+            let type = pathAPI.split('.')[1]
+
+
+            const url = pathAPI
+            const body = await axios.get(url, {
+                responseType: 'arraybuffer'
+            });
+            const data = body.data;
+            await zip.addFile(`attachment_${index}.${type}`, Buffer.from(data, "utf8"), "", 0644 << 16)
+            index++
+        }
+
+        zip.toBuffer();
         zip.writeZip(`public/zip/${file_name}`)
 
         const directoryPath = cwd()
@@ -89,7 +99,6 @@ module.exports = {
         const response = fs.createReadStream(pathZip);
         ctx.response.set("content-type", 'application/zip');
         ctx.response.set("content-disposition", `attachment; filename=${file_name}`);
-        const downloadUrl = `${process.env.BASE_URL}/zip/${file_name}`
-        ctx.body = downloadUrl;
+        ctx.body = response;
     }
 }
